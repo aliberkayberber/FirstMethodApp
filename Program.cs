09357dc0d22a9d7aeac8d64da887f325952004c1
aliberkayberber <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>
2024-07-19 14:10:39 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FirstMethodApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingSong();
+            System.Console.WriteLine("2 ye bölümünden kalan: " + RandomNumber());
+            System.Console.WriteLine("Çarpma işleminin sonucu: " + MultiplicationNumber(5,9));
+            Greeting("Berkay","Berber");
+        }
+
+        static void SingSong() 
+        {
+            System.Console.WriteLine("Heyecandan her gece hayalini \nUnutursam ağlama unut beni");
+        }
+        static int RandomNumber() 
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1,100);
+            System.Console.WriteLine("Random sayi: " + number);
+            return (number % 2);
+        }
+
+        static int MultiplicationNumber(int x , int y)
+        {
+            return (x*y);
+        }
+
+        static void Greeting(string name, string surname) 
+        {
+            System.Console.WriteLine($"Hoş Geldiniz {name} {surname}");
+        }
+    }
+
+}
